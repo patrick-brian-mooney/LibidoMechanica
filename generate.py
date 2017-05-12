7de7830d87507b2dfaefaa61c454d4a679ff43ea
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-05-12 14:43:34 -0700

    12 May 2017: we're up and running

--- generate.py
+++ generate.py
@@ -16,7 +16,7 @@ GPL, either version 3, or (at your option) any later version; see the file
 LICENSE.md for more details.
 """
 
-import random, pprint, glob, os, datetime, json
+import random, pprint, glob, os, datetime, json, bz2
 
 import patrick_logger                                   # From https://github.com/patrick-brian-mooney/personal-library
 import social_media                                     # From https://github.com/patrick-brian-mooney/personal-library
@@ -40,51 +40,66 @@ def print_usage():    # Note that, currently, nothing calls this.
 
 def get_title(the_poem):
     """Get a title for the poem."""
-    return "Untitled Composition # %d" % len(glob.glob(post_archives))
+    possible_titles = [
+      lambda: "Untitled Poem # %d" % (1 + len(glob.glob(post_archives + '/*Untitled*'))),
+      lambda: "Untitled Poem # %d" % (1 + len(glob.glob(post_archives + '/*Untitled*'))),
+      lambda: th.strip_leading_and_trailing_punctuation("‘%s’" % the_poem.split('\n')[0].strip()).strip(),  # First line, in quotes 
+      lambda: th.strip_leading_and_trailing_punctuation("‘%s’" % the_poem.split('\n')[0].strip()).strip(),  # First line, in quotes 
+      lambda: th.strip_leading_and_trailing_punctuation("‘%s’" % the_poem.split('\n')[0].strip()).strip(),  # First line, in quotes 
+      lambda: genny.gen_text(sentences_desired=1).replace('\n', ' ').strip()[:-1], # New 'sentence' from corpus 
+    ]
+    title = random.choice(possible_titles)()
+    while len(title) > 120:
+        words = title.split()
+        title = ' '.join(words[:random.randint(3, min(12, len(words)))])
+    return title
 
-if __name__ == "__main__":
-    
-    # Set up the basic parameters for the run
-    sample_texts = random.sample(glob.glob(poetry_corpus + '/*txt'), random.randint(6,20))
-    chain_length = random.randint(3,7)
-    
-    # And add their names to the list of tags
-    the_tags =  [ os.path.splitext(th.remove_prefix(os.path.basename(title), "Link to ").strip())[0] for title in sample_texts ] + \
-                ['poetry', 'automatically generated text', 'Patrick Mooney', 'Python', 'Markov chains'] + \
-                ['Markov chain length: %d' % chain_length ]
-    
-    poem_length = random.randint(4,12)              # in SENTENCES. Not lines.
-    
-    genny = pg.PoemGenerator(name='Libido Mechanica generator', training_texts=sample_texts, markov_length=chain_length)
-        
-    the_poem = genny.gen_text(sentences_desired=poem_length, paragraph_break_probability=0.2)
-    the_title=get_title(the_poem)
-    
-    formatted_poem = th.multi_replace(the_poem, [[' \n', '\n'],               # Eliminate any spurious end-of-line spaces
-                                                 ['\n\n\n', '\n\n']])         # ... and any extra line breaks.
-    
-    # FIXME: preserve beginning-of-line spacing
-    
-    # Add HTML <br /> to end of every line
-    formatted_poem = '\n'.join([line.rstrip() + '<br />' for line in formatted_poem.split('\n')])
-    # Wrap stanzas in <p> ... </p>
-    formatted_poem = '\n'.join(['<p>%s</p>' % line for line in formatted_poem.split('<br />\n<br />')])
-    # Pretty-print for debugging only
-    formatted_poem = th.multi_replace(formatted_poem, [['<p>\n', '\n<p>']])
-    # Get rid of spurious paragraphs    
-    formatted_poem = th.multi_replace(formatted_poem, [['<p></p>', '']])
-    
-    patrick_logger.log_it("poem generated; title is; %s" % the_title)
-    patrick_logger.log_it("lines are: \n\n" + the_poem)
-    patrick_logger.log_it("tags are: %s" % the_tags)
-    
-    # All right, we're ready. Let's go.
-    patrick_logger.log_it('INFO: Attempting to post the content', 2)
-    the_status = social_media.tumblr_text_post(libidomechanica_client, ', '.join(the_tags), the_title, the_poem)
-    patrick_logger.log_it('INFO: the_status is: ' + pprint.pformat(the_status), 2)
-    
-    # Archive the generated post
-    with open(os.path.join(post_archives, datetime.datetime.now().isoformat() + '.json'), 'w') as archive_file:
-        json.dump(the_status, archive_file)
-    
-    patrick_logger.log_it("INFO: We're done", 1)
+
+# Set up the basic parameters for the run
+sample_texts = random.sample(glob.glob(poetry_corpus + '/*txt'), random.randint(5,12))
+chain_length = random.randint(3,7)
+
+# And add their names to the list of tags
+the_tags =   [ os.path.splitext(th.remove_prefix(os.path.basename(t), "Link to ").strip())[0] for t in sample_texts ]
+the_tags =   [ t.replace(',', ':').replace('"', '').replace("'", '') for t in the_tags ]
+the_tags +=  ['poetry', 'automatically generated text', 'Patrick Mooney', 'Markov chains'] + \
+             ['Markov chain length: %d' % chain_length, '%d texts' % len(sample_texts) ]
+
+poem_length = random.randint(4,20)              # in SENTENCES. Not lines.
+
+genny = pg.PoemGenerator(name='Libido Mechanica generator', training_texts=sample_texts, markov_length=chain_length)
+
+the_poem = genny.gen_text(sentences_desired=poem_length, paragraph_break_probability=0.2)
+the_title=get_title(the_poem)
+
+formatted_poem = th.multi_replace(the_poem, [[' \n', '\n'],               # Eliminate any spurious end-of-line spaces
+                                             ['\n\n\n', '\n\n']])         # ... and any extra line breaks.
+
+# FIXME: preserve beginning-of-line spacing
+
+# Add HTML <br /> to end of every line
+formatted_poem = '\n'.join([line.rstrip() + '<br />' for line in formatted_poem.split('\n')])
+# Wrap stanzas in <p> ... </p>
+formatted_poem = '\n'.join(['<p>%s</p>' % line for line in formatted_poem.split('<br />\n<br />')])
+# Pretty-print (for debugging only; doesn't matter for Tumblr upload, but neither does it cause problems)
+formatted_poem = th.multi_replace(formatted_poem, [['<p>\n', '\n<p>']])
+# Prevent all spaces from collapsing; get rid of spurious paragraphs
+formatted_poem = th.multi_replace(formatted_poem, [[' ', '&nbsp;'], ['<p></p>', '']])
+
+patrick_logger.log_it("poem generated; title is; %s" % the_title)
+patrick_logger.log_it("lines are: \n\n" + the_poem)
+patrick_logger.log_it("tags are: %s" % the_tags)
+
+# All right, we're ready. Let's go.
+patrick_logger.log_it('INFO: Attempting to post the content', 2)
+the_status, the_tumblr_data = social_media.tumblr_text_post(libidomechanica_client, ', '.join(the_tags), the_title, the_poem)
+patrick_logger.log_it('INFO: the_status is: ' + pprint.pformat(the_status), 2)
+
+# Archive the generated post
+post_data = {'title': the_title, 'text': the_poem, 'time': datetime.datetime.now().isoformat() }
+post_data['status_code'], post_data['tumblr_data'], post_data['tags'] = the_status, the_tumblr_data, the_tags
+archive_name = "%s — %s.json.bz2" % (post_data['time'], the_title)
+with bz2.BZ2File(os.path.join(post_archives, archive_name), mode='wb') as archive_file:
+    archive_file.write(json.dumps(post_data, sort_keys=True, indent=3, ensure_ascii=False).encode())
+
+patrick_logger.log_it("INFO: We're done", 1)
