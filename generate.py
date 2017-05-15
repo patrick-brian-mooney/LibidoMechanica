be6de042494d635da1a90392772e8a533f050f47
Patrick Brian Mooney <github.pmooney78@neverbox.com>
2017-05-15 16:17:35 -0700

    15 May 2017: adding new poems to training corpus, plus tweaking script parameters

--- generate.py
+++ generate.py
@@ -70,7 +70,7 @@ def get_title(the_poem):
 
 
 # Set up the basic parameters for the run
-sample_texts = random.sample(glob.glob(poetry_corpus + '/*txt'), random.randint(20,50))
+sample_texts = random.sample(glob.glob(poetry_corpus + '/*txt'), random.randint(40,100))
 chain_length = random.choice([3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 8, 9, 10])
 
 # And add their names to the list of tags, plus track sources of this particular poem
